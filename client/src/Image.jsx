de5e36db5319f1f2775c65b82335438f806efbf7
Anand Mainali <anand.mainali@cyberarrow.io>
2024-01-07 12:59:17 +0545

    minor changes

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Image = () => {
+    const [prompt,setPropmt] = useState('')
+    const handleSubmit = ()=>{
+        
+    }
   return (
     <div className='w-full h-screen bg-pink-300 flex'>
-        <div className='max-w-[80%] w-full m-auto flex flex-col gap-10 justify-center items-center'>
-            <h1 className='text-3xl sm:text-4xl font-[900] text-[#ca0b2b]'>Generate Image Using AI</h1>
+        <div className='max-w-[80%] w-full m-auto flex flex-col gap-10'>
+            <h1 className='text-2xl sm:text-4xl font-[900] text-[#ca0b2b] text-center sm:text-left'>Generate Image Using AI</h1>
             <img width={500} src="girl.webp" alt="girl" className='rounded-lg '/>
-            <form className='w-full flex flex-col gap-2 '>
-                <label className='text-xl sm:text-3xl text-[#d3435b] text-center'>What do you want to create?</label>
-                <input type="text"  id="text" placeholder='type here' className='px-4 py-2 rounded-lg bg-[#eb89b7] text-white outline-none border-2px border-[#a86671] text-2xl text-center'/>
+            <form onSubmit={handleSubmit} className='w-full flex flex-col gap-3 '>
+                <label className='text-xl sm:text-3xl text-[#d3435b] text-center sm:text-left'>What do you want to create?</label>
+                <input type="text"  id="text" placeholder='type here' className='px-4 py-2 rounded-lg bg-[#eb89b7] text-white outline-none border-2px border-[#a86671] text-2xl' onChange={(e)=>setPropmt(e.target.value)}/>
                 <button className='px-4 py-2 rounded-lg bg-[#eb89b7] text-2xl font-bold text-[#ec7689]'>Generate</button>
             </form>
         </div>
